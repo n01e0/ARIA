f7ddc9522a0f599a4c07b06e5d29550790cda208
n01e0 <noleo@vanillabeans.mobi>
2020-04-06 06:18:26 +0900

    時は現代。売れないお笑いコンビ｢ラブフェロモン｣は、日々大スターになることを夢見ていた。そんな二人には大きな切り札があった。実は、二人はもう一つの顔を持っていたのだ!それは……正義の味方(笑)!｢代々科学を利用し戦ってきた正義の味方の血を生かせば世界的に有名になれる!そうしたら、お笑いでブロードウェイに立つことも夢じゃない!｣かくして、漫才活動のかたわら“正義の味方”をやり、世間に名前を売ろうとするラブフェロモン!しかし、二人の戦いはムチャクチャなものばっかり…。自分たちで勝手に悪を決めつけ、｢正義のためならなにをしてもいいのだぁ!(by良澄愛美)｣という大義名分のもとに、暴れまくるのだが……。

--- tests/test_instruction.rs
+++ tests/test_instruction.rs
@@ -40,7 +40,61 @@ mod instruction {
         instructions(emu.get_code8(0)).unwrap()(&mut emu);
         assert_eq!(emu.registers[0] as u32, 0x01234567 as u32);
     }
+
+    #[test]
+    fn instruction_mov_rm32_imm32() {
+        let mut emu = Emulator {
+            registers: [0, 0, 0, 0, 0, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0xC7, 0xC0, 0x00, 0x00, 0x00, 0x00],
+            eip: 0,
+        };
+
+        emu.set_memory32(2, 0x01234567);
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.registers[0], 0x01234567);
+    }
     
+    #[test]
+    fn instruction_mov_rm32_r32() {
+        let mut emu = Emulator {
+            registers: [2, 0, 0, 0, 0, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0x89, 0x00, 0x00, 0x00, 0x00, 0x00],
+            eip: 0,
+        };
+
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.registers[0], emu.get_memory32(2));
+    }
+
+    #[test]
+    fn instruction_mov_r32_rm32() {
+        let mut emu = Emulator {
+            registers: [0, 2, 0, 0, 0, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0x8B, 0x11, 0x00, 0x00, 0x00, 0x00],
+            eip: 0,
+        };
+
+        emu.set_memory32(2, 0x12345678);
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.registers[2], emu.get_memory32(2));
+    }
+
+    #[test]
+    fn instruction_add_rm32_r32() {
+        let mut emu = Emulator {
+            registers: [0, 0xF0, 0x0F, 0, 0, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0x01, 0b11010001, 0x00],
+            eip: 0,
+        };
+
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.registers[1], 0xFF);
+    }
+
     #[test]
     fn instruction_sub_rm32_imm8() {
         let mut emu = Emulator {
@@ -51,8 +105,145 @@ mod instruction {
         };
         
         instructions(emu.get_code8(0)).unwrap()(&mut emu);
-        assert_eq!(&format!("{:X}", emu.registers[0x4]), "E0");
         assert_eq!(emu.get_register32(0x4), 0xE0);
     }
     
+    #[test]
+    fn instruction_inc_rm32() {
+        let mut emu = Emulator {
+            registers: [0, 0, 0, 0, 0, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0xFF, 0b11000111],
+            eip: 0,
+        };
+
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.registers[7], 1);
+    }
+
+    #[test]
+    fn instruction_push_r32() {
+        let mut emu = Emulator {
+            registers: [0, 0xFF, 0, 0, 0x5, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0x51, 0x00, 0x00, 0x00, 0x00, 0x00],
+            eip: 0,
+        };
+
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.get_memory32(1), 0xFF);
+    }
+
+    #[test]
+    fn instruction_push_imm32() {
+        let mut emu = Emulator {
+            registers: [0, 0, 0, 0, 0x8, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0x68, 0, 0, 0, 0, 0, 0, 0, 0],
+            eip: 0,
+        };
+
+        emu.set_memory32(1, 0x12345678);
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.get_memory32(4), 0x12345678);
+    }
+
+    #[test]
+    fn instruction_push_imm8() {
+        let mut emu = Emulator {
+            registers: [0, 0, 0, 0, 0x6, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0x6A, 0, 0, 0, 0, 0],
+            eip: 0,
+        };
+
+        emu.set_memory8(1, 0xFF);
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.get_memory32(2), 0xFF);
+    }
+
+    #[test]
+    fn instruction_pop_r32() {
+        let mut emu = Emulator {
+            registers: [0, 0, 0, 0, 1, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0x58, 0, 0, 0, 0],
+            eip: 0,
+        };
+
+        emu.set_memory32(1, 0x12345678);
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.registers[0], 0x12345678);
+    }
+
+    #[test]
+    fn instruction_short_jump() {
+        let mut emu = Emulator {
+            registers: [0, 0, 0, 0, 0, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0xEB, 0xFF],
+            eip: 0,
+        };
+
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.eip, 1);
+    }
+
+    #[test]
+    fn instruction_near_jump() {
+        let mut emu = Emulator {
+            registers: [0, 0, 0, 0, 0, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0xE9, 0, 0, 0, 0],
+            eip: 0,
+        };
+
+        emu.set_memory32(1, 0x12345673);
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.eip, 0x12345678);
+    }
+
+    #[test]
+    fn instruction_call_rel32() {
+        let mut emu = Emulator {
+            registers: [0, 0, 0, 0, 5, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0xE8, 0, 0, 0, 0],
+            eip: 0,
+        };
+
+        emu.set_memory32(1, 0x12345673);
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.eip, 0x12345678);
+        assert_eq!(emu.get_memory32(1), 5);
+    }
+
+    #[test]
+    fn instruction_ret() {
+        let mut emu = Emulator {
+            registers: [0, 0, 0, 0, 1, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0xC3, 0, 0, 0, 0],
+            eip: 0,
+        };
+
+        emu.set_memory32(1, 0x12345678);
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.eip, 0x12345678);
+    }
+
+    #[test]
+    fn instruction_leave() {
+        let mut emu = Emulator {
+            registers: [0, 0, 0, 0, 0, 0, 0, 0],
+            eflags: 0,
+            memory: vec![0xC9, 0, 0, 0, 0],
+            eip: 0,
+        };
+
+        emu.set_register32(Register::EBP as usize, 1);
+        emu.set_memory32(1, 0x12345678);
+        instructions(emu.get_code8(0)).unwrap()(&mut emu);
+        assert_eq!(emu.registers[Register::EBP as usize], 0x12345678);
+    }
 }
